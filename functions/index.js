3402fd9c3b9711dc0c0eaa9d302d8a0b26e5b6aa
Lauren McCarthy <laurenleemccarthy@gmail.com>
2021-11-21 20:56:24 +0100

    ok

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,28 @@
 const functions = require('firebase-functions');
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+const cors = require('cors')({origin: true});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-exports.left = functions.https.onRequest((request, response) => {
-  client.channels.get('757688700011282608').send('LEFT');
-  response.send('success');
+exports.left = functions.https.onRequest((req, res) => {
+  // cors(req, res, () => {
+    client.channels.get('757688700011282608').send('LEFT');
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send('success');
+  // });
+
 });
 
-exports.right = functions.https.onRequest((request, response) => {
-  client.channels.get('757688700011282608').send('RIGHT');
-  response.send('success');
+exports.right = functions.https.onRequest((req, res) => {
+
+  // cors(req, res, () => {
+    client.channels.get('757688700011282608').send('RIGHT');
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send('success');
+  // });
 });
 
 client.login(functions.config().discord.token);
